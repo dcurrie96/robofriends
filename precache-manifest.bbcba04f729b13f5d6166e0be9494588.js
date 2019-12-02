8161605283fd7457c5ea232d408540e2c11fceea
Daniel Currie <danielgcurrie@gmail.com>
2019-12-02 16:35:47 -0600

    Updates

---
+++ precache-manifest.bbcba04f729b13f5d6166e0be9494588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d0c100437dd422e52ea6206d0cc4df",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "deee328ca901c33ee23c",
+    "url": "/robofriends/static/css/main.63088e06.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/robofriends/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "deee328ca901c33ee23c",
+    "url": "/robofriends/static/js/main.3b92502b.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
